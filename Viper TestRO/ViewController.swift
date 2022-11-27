bd6a645671f2181e93c5b0bd3ef8a3124a4a6ee5
Stanislav <kaggy8@ya.ru>
2022-11-28 00:09:22 +0300

    Initial Commit

---
+++ Viper TestRO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Viper TestRO
+//
+//  Created by Stanislav Demyanov on 28.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
