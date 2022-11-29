48c35d504b44ffea8f6a6a5c26f55e473d4d66fc
Stanislav <kaggy8@ya.ru>
2022-11-30 02:10:25 +0300

    add TodoDetailRouter

---
+++ Viper TestRO/TodoDetailModule/Router/TodoDetailRouter.swift
@@ -0,0 +1,40 @@
+//
+//  TodoDetailRouter.swift
+//  Viper TestRO
+//
+//  Created by Stanislav Demyanov on 30.11.2022.
+//
+
+import UIKit
+
+class TodoDetailRouter: TodoDetailRouterProtocol {
+    
+    static var storyboard: UIStoryboard {
+        return UIStoryboard(name: "Main", bundle: Bundle.main)
+    }
+    
+    func navigateBackToListViewController(from view: TodoDetailViewProtocol) {
+        guard let viewVC = view as? UIViewController else {
+            fatalError("Invalid View Protocol Type")
+        }
+        viewVC.navigationController?.popViewController(animated: true)
+    }
+    
+    static func createTodoDetailRouterModule(with todo: TodoItem) -> UIViewController {
+        
+        guard let todoDetailVC = storyboard.instantiateViewController(withIdentifier: "TodoDetailViewController") as? TodoDetailViewController else {
+            fatalError("Invalid View Protocol Type")
+        }
+        
+        let presenter: TodoDetailPresenter & TodoDetailInteractorOutputProtocol = TodoDetailPresenter()
+        todoDetailVC.presenter = presenter
+        presenter.view = todoDetailVC
+        let interactor: TodoDetailInteractorInputProtocol = TodoDetailInteractor()
+        interactor.todoItem = todo
+        interactor.presenter = presenter
+        let router: TodoDetailRouterProtocol = TodoDetailRouter()
+        presenter.router = router
+        
+        return todoDetailVC
+    }
+}
